2d336646c70b2add63160a8e3aec58e717641f62
__.flxk.__ <89276072+fluk3G123@users.noreply.github.com>
2023-02-13 20:50:35 +0700

    compro

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <cmath>
+#include <numeric>
+#include <algorithm>
+
 using namespace std;
 
 void stat(const double[],int,double[]);
@@ -16,4 +19,29 @@ int main(){
     cout << "\nMax = " << B[4];
     cout << "\nMin = " << B[5];
     return 0;
+}
+
+void stat(const double A[],int N,double B[]){
+    double mean = 0;
+    for (int i = 0; i < N; i++) {
+        mean += A[i];}
+    B[0] = mean / N;
+
+double variance = 0;
+    for (int i = 0; i < N; i++) {
+        variance += (A[i] - B[0]) * (A[i] - B[0]);}
+    B[1] = sqrt(variance / N);
+
+double GM = 1;
+    for (int i = 0; i < N; i++) {
+        GM *= A[i];}
+    B[2] = pow(GM, 1.0 / N);
+
+double HM = 0.0;
+    for (int i = 0; i < N; i++) {
+        HM += 1.0 / A[i];}
+    B[3] = N / HM;
+
+B[4] = *max_element(A, A + N);
+B[5] = *min_element(A, A + N);
 }
